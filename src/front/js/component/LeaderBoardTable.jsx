4138c6eee2935ae3a83c6d1c2f91eee209cd6d0c
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2025-03-19 17:19:19 +0000

    Implementar componente tabla de puntuaciones

---
+++ src/front/js/component/LeaderBoardTable.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { getBestFiveGames, getInfoUser, getLastFiveGames, getMinigameById } from "../../services/APIServices";
+
+const LeaderBoardTable = ({ minigameId, userId }) => {
+    const [leaderBoardData, setLeaderBoardData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchLeaderBoard = async () => {
+            try {
+                let data;
+
+                if (userId) {
+                    data = await getLastFiveGames(userId);
+                } else {
+                    data = await getBestFiveGames(minigameId);
+                }
+
+                const updateData = await Promise.all(
+                    data.map(async (entry) => {
+                        const userInfo = await getInfoUser(entry.user_id);
+                        const minigameInfo = await getMinigameById(minigameId);
+                        return {
+                            ...entry,
+                            user_name: userInfo ? userInfo.user_name : "Desconocido",
+                            minigameName: minigameInfo ? minigameInfo.title : "Desconocido",
+                        };
+                    })
+                )
+
+                setLeaderBoardData(updateData);
+            } catch (error) {
+                console.error("Error al obtener las leadderboards:", error);
+            } finally {
+                setLoading(false);
+            }
+
+
+        }
+
+        fetchLeaderBoard();
+    }, [])
+
+    return (
+        <div className="border rounded p-3" style={{ backgroundColor: "#001833" }}>
+            <h2 className="text text-light">Leader Board</h2>
+            {loading ? (
+                <p className="text text-light">Cargando resultados...</p>
+            ) : (
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th className="text text-light">Rank</th>
+                            <th className="text text-light">Usuario</th>
+                            <th className="text text-light">Minijuego</th>
+                            <th className="text text-light">Puntuación</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.isArray(leaderBoardData) && leaderBoardData.length > 0 ? (
+                            leaderBoardData.map((entry, index) => (
+                                <tr key={index}>
+                                    <td className="text text-light">{index + 1}</td>
+                                    <td className="text text-light">{entry.user_name}</td>
+                                    <td className="text text-light">{entry.minigameName}</td>
+                                    <td className="text text-light">{entry.game_points}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <td colSpan="4" className="text-center text-light">
+                                No hay datos disponibles
+                            </td>
+                        )}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    )
+}
+
+export default LeaderBoardTable;
